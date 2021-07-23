b7e9980414e6455eb64bb939c0dd6201290cdd27
Steven <steventhomson72@yahoo.ca>
2021-07-23 19:00:03 -0400

    added calls to scrape data from input url

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import asyncio
 from metadata import RestMetadata
+from scraping import fetch_query
 import sys
+from csv import reader, writer, QUOTE_MINIMAL
 
 
 async def main():
@@ -9,7 +11,20 @@ async def main():
         "/FeatureServer/0"
     )
     print(metadata.json_text)
-
+    if input("Proceed with scrape? (y/n)").upper() == "Y":
+        tasks = (fetch_query(query, metadata) for query in metadata.queries)
+        with open(f"{metadata.name}.csv", newline="") as output_file:
+            csv_writer = writer(output_file, delimter=",", quotechar='"', quoting=QUOTE_MINIMAL)
+            csv_writer.writerow(metadata.fields)
+            for file in await asyncio.gather(*tasks):
+                with open(file, newline="") as csv_file:
+                    csv_writer.writerows(
+                        reader(
+                            csv_file,
+                            delimiter=",",
+                            quotechar='"'
+                        )
+                    )
 
 if __name__ == "__main__":
     if sys.platform in ("win32", "cygwin"):
